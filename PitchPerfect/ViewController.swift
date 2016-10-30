dfe9bbe869f8f33d67e0bd352daec500d24564bb
Max Schwaab <schwaabm@amazon.com>
2016-10-30 22:44:14 +0100

    added images to buttons enabled and disabled buttons based on state

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var abc: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,9 +25,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        stopRecordingButton.isEnabled = false;
+    }
+    
     @IBAction func recordAudio(_ sender: Any) {
         print("record button pressed")
+        stopRecordingButton.isEnabled = true;
+        recordButton.isEnabled = false;
+        recordingLabel.text = "Recording in progress"
     }
 
+    @IBAction func stopRecording(_ sender: Any) {
+        print("stop recording button pressed")
+        stopRecordingButton.isEnabled = false;
+        recordButton.isEnabled = true;
+        recordingLabel.text = "Tap to Record"
+    }
+    
 }
 
